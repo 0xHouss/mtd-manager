9930afd89dd6f8c67204c08f1b6c4d389d0de582
0xHouss <tkthoussam@gmail.com>
2024-09-21 21:35:46 +0100

    add gbp handling

--- src/commands/forex.ts
+++ src/commands/forex.ts
@@ -3,14 +3,8 @@ import { startBrowser } from '../lib/puppeteer';
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('black-to-white')
-        .setDescription('Get the white amount of money to receive')
-        .addIntegerOption(option =>
-            option
-                .setName('black-amount')
-                .setDescription('The amount in black')
-                .setRequired(true)
-        ),
+        .setName('forex')
+        .setDescription('Get the currency prices'),
     async execute(interaction: ChatInputCommandInteraction) {
         if (interaction.member?.permissions instanceof PermissionsBitField) {
             const hasPermission = interaction.member.permissions.has(PermissionsBitField.Flags.Administrator);
@@ -28,28 +22,30 @@ export default {
             ], ephemeral: true
         })
 
-        const blackAmount = interaction.options.getInteger('black-amount')!
-
         await interaction.deferReply({ ephemeral: true })
 
         const { browser, page } = await startBrowser()
 
         await page.goto("http://www.forexalgerie.com")
 
-        const blackPrice = await page.evaluate(() => +document.querySelector<HTMLTableCellElement>("#eurBuy")?.innerText!)
+        const eurBlack = await page.evaluate(() => +document.querySelector<HTMLTableCellElement>("#eurBuy")?.innerText!)
+        const gbpBlack = await page.evaluate(() => +document.querySelector<HTMLTableCellElement>("#gbpBuy")?.innerText!)
 
         await page.goto("https://fcsapi.com/converter/eur/dzd")
 
-        const whitePrice = Math.floor(await page.evaluate(() => +document.querySelector<HTMLSpanElement>(".converterresult-toAmount")?.innerText!))
+        const eurWhite = Math.floor(await page.evaluate(() => +document.querySelector<HTMLSpanElement>(".converterresult-toAmount")?.innerText!))
 
-        const whiteAmount = blackAmount * blackPrice / whitePrice
+        await page.goto("https://fcsapi.com/converter/gbp/dzd")
+
+        const gbpWhite = Math.floor(await page.evaluate(() => +document.querySelector<HTMLSpanElement>(".converterresult-toAmount")?.innerText!))
 
         const embed = new EmbedBuilder()
-            .setTitle("Black to White Convertor")
-            .setDescription(`To receive ${blackAmount}€ in black you need to receive ${Math.floor(whiteAmount)}€ in white`)
+            .setTitle("Forex Prices")
             .addFields(
-                { name: "White Price of 1€:", value: whitePrice + ' DZD', inline: true },
-                { name: "Black Price of 1€:", value: blackPrice + ' DZD', inline: true }
+                { name: "White Price of 1€:", value: eurWhite + ' DZD', inline: true },
+                { name: "Black Price of 1€:", value: eurBlack + ' DZD', inline: true },
+                { name: "White Price of 1£:", value: gbpWhite + ' DZD', inline: false },
+                { name: "Black Price of 1£:", value: gbpBlack + ' DZD', inline: true },
             )
             .setColor("Blue")
 
