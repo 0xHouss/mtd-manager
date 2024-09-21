d36287764784a93b4b6e35ec2eb1a20f08260903
0xHouss <tkthoussam@gmail.com>
2024-09-21 21:16:05 +0100

    verify perms of /black-to-white

--- src/commands/black-to-white.ts
+++ src/commands/black-to-white.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, Embed, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, PermissionsBitField, SlashCommandBuilder } from 'discord.js';
 import { startBrowser } from '../lib/puppeteer';
 
 export default {
@@ -12,6 +12,22 @@ export default {
                 .setRequired(true)
         ),
     async execute(interaction: ChatInputCommandInteraction) {
+        if (interaction.member?.permissions instanceof PermissionsBitField) {
+            const hasPermission = interaction.member.permissions.has(PermissionsBitField.Flags.Administrator);
+
+            if (!hasPermission) return await interaction.reply({
+                embeds: [new EmbedBuilder()
+                    .setTitle("You can't use this command !")
+                    .setColor("Red")
+                ], ephemeral: true
+            })
+        } else return await interaction.reply({
+            embeds: [new EmbedBuilder()
+                .setTitle("You can't use this command !")
+                .setColor("Red")
+            ], ephemeral: true
+        })
+
         const blackAmount = interaction.options.getInteger('black-amount')!
 
         await interaction.deferReply({ ephemeral: true })
@@ -26,16 +42,14 @@ export default {
 
         const whitePrice = Math.floor(await page.evaluate(() => +document.querySelector<HTMLSpanElement>(".converterresult-toAmount")?.innerText!))
 
-        console.log(blackPrice, whitePrice);
-
         const whiteAmount = blackAmount * blackPrice / whitePrice
 
         const embed = new EmbedBuilder()
             .setTitle("Black to White Convertor")
             .setDescription(`To receive ${blackAmount}€ in black you need to receive ${Math.floor(whiteAmount)}€ in white`)
             .addFields(
-                { name: "White Price of 1€:", value: whitePrice+' DZD', inline: true },
-                { name: "Black Price of 1€:", value: blackPrice+' DZD', inline: true }
+                { name: "White Price of 1€:", value: whitePrice + ' DZD', inline: true },
+                { name: "Black Price of 1€:", value: blackPrice + ' DZD', inline: true }
             )
             .setColor("Blue")
 
